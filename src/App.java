09736c03c171d5bc0d470096fc029b4561aa9d3f
Bruno Marques <brunorn10@gmail.com>
2023-04-03 17:25:14 -0300

    Adding code to make a request for imdb-api

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        // extrair apenas os dados que interessam (título, poster, classificação)
+
+        // manipular e exibir os dados
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI address  = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
     }
 }
