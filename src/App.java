ec30240370f6691eabd1ebfeca945c4ce3f1a62d
Bruno Marques <brunorn10@gmail.com>
2023-04-04 09:53:23 -0300

    Added JsonParser

--- src/App.java
+++ src/App.java
@@ -3,22 +3,30 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
-
-        // extrair apenas os dados que interessam (título, poster, classificação)
-
-        // manipular e exibir os dados
-
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI address  = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        // extrair apenas os dados que interessam (título, poster, classificação)
+        JsonParser jParser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = jParser.parse(body);
+
+        // manipular e exibir os dados
+        for (Map<String,String> filme : listaDeFilmes)
+        {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
     }
 }
